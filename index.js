ad0f0ebb87a294c4beb3a7728974d1e5f6a75a91
Alena Trushnikova <alena.yu.t@gmail.com>
2021-01-13 09:45:29 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+let dodger = document.getElementById('dodger')
+
+// moves the DODGER to the left
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+})
+
+// moves the DODGER to the right
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+})
